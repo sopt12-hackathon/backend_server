7341215369f76bcaa830db91b66a57491e960a60
epitone <epitoneplus@gmail.com>
2020-12-16 19:05:11 +0900

    feat: Routing 진행 중 Route: 기본 라우트 설정

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+router.use("/user", require("./users"));
+router.use("/recommend", require("./recommend"));
+router.use("/like", require("./like"));
+
 module.exports = router;
---
+++ routes/like/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+const likeController = require("../../controller/likeController");
+const isLoggedIn = require("../util/isLoggedIn");
+
+router.post("/", isLoggedIn, likeController.saveLike);
+module.exports = router;
