a3a8f32c0b5df5f1b16811820398cfa18e93a71c
epitone <epitoneplus@gmail.com>
2020-12-16 11:05:34 +0900

    빈 커밋

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
